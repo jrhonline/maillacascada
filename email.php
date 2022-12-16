718bdb7fc3aac10f27a67954fd6130a4d57e737d
jrhonline <116745127+jrhonline@users.noreply.github.com>
2022-12-16 13:58:02 -0600

    Update email.php

--- email.php
+++ email.php
@@ -1,1 +1,11 @@
-html
+<?php
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = "xjosuerafaelx@gmail.com";
+    $to = "josue.hernandez@lacascada.com.sv";
+    $subject = "Checking PHP mail";
+    $message = "PHP mail works just fine";
+    $headers = "From:" . $from;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
+?>
